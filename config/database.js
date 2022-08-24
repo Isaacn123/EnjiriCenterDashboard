32f639719fbb7ea94ad8a89166d27001e882723f
isaacn123 <nsambai72@gmail.com>
2022-08-25 01:22:10 +0300

    database url

--- config/database.js
+++ config/database.js
@@ -1,43 +1,29 @@
-// const parse = require('pg-connection-string').parse;
-
+const parse = require('pg-connection-string').parse;
+const config = parse(process.env.DATABASE_URL);
 module.exports = ({ env }) => {
   
   if(env('NODE_ENV') === 'production'){
     // const config = parse(process.env.DATABASE_URL);
-   
+
     return {
       defaultConnection:'default',
       connections:{
         default:{
           connector: 'bookshelf',
           settings: {
-            client: 'mysql',
-            host: process.env.DATABASE_HOST,
-            port: process.env.DATABASE_PORT,
-            database: process.env.DATABASE_NAME,
-            username: process.env.DATABASE_USERNAME,
-            password: process.env.DATABASE_PASSWORD,
+            client: 'postgres',
+            host:config.host,
+            port:config.port,
+            database:config.database,
+            user:config.user,
+            password:config.password,
           },
           ssl: {
-            rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+            rejectUnauthorized: false, // For self-signed certificates
           },
           options: {
              useNullAsDefault: true,
           },
-          // settings: {
-          //   client: 'postgres',
-          //   host: config.host,
-          //   port: config.port,
-          //   database: config.database,
-          //   username: config.user,
-          //   password: config.password,
-          //   ssl: {
-          //     rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
-          //   },
-          // },
-          // options: {
-          //   ssl: env.bool('DATABASE_SSL', false),
-          // },
         }
       }
     }
