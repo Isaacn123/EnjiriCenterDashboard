cf6968be5b8dcd411d7b847cc6792eb56b41973b
isaacn123 <nsambai72@gmail.com>
2022-08-25 14:25:02 +0300

    upload

--- config/plugins.js
+++ config/plugins.js
@@ -41,6 +41,18 @@ module.exports = ({ env }) => {
         // },
   
     }
+  }else{
+    return  { 
+       upload:{
+            provider:'cloudinary',
+            providerOptions:{
+                cloud_name:env('CLOUDINARY_NAME'),
+                api_key: env('CLOUDINARY_KEY'),
+                api_secret: env('CLOUDINARY_SECRET'),
+                // upload_preset: env('CLOUDINARY_UPLOAD_PRESET'),
+            } 
+          }
+          };
   }
 
 
