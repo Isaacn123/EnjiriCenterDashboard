775ad2dc36df48833c1e1418e05f6f6889edc605
isaacn123 <nsambai72@gmail.com>
2022-08-25 16:30:17 +0300

    cloud

--- config/plugins.js
+++ config/plugins.js
@@ -2,24 +2,12 @@ module.exports = ({ env }) => {
 
   if(env('NODE_ENV') === 'production'){
     return  {
-      // ...
-      // upload: {
-      //   provider: 'aws-s3',
-      //   providerOptions: {
-      //     accessKeyId: env('AWS_ACCESS_KEY_ID'),
-      //     secretAccessKey: env('AWS_ACCESS_SECRET'),
-      //     region: env('AWS_REGION'),
-      //     params: {
-      //       Bucket: env('AWS_BUCKET'),
-      //     },
-      //   },
-      // },
+
       upload: {
         provider: 'uploadcare',
         providerOptions: {
           public_key: env('UPLOADCARE_PUBLIC_KEY'),
           secret_key: env('UPLOADCARE_SECRET_KEY'),
-          // base_cdn: env('UPLOADCARE_BASE_CDN'),
         },
       },
       // upload:{
@@ -41,6 +29,16 @@ module.exports = ({ env }) => {
         // },
   
     }
+  } else{
+    return {
+      upload: {
+        provider: 'uploadcare',
+        providerOptions: {
+          public_key: env('UPLOADCARE_PUBLIC_KEY'),
+          secret_key: env('UPLOADCARE_SECRET_KEY'),
+        },
+      }
+    };
   }
   // else{
   //   return  { 
@@ -81,25 +79,13 @@ module.exports = ({ env }) => {
 
 module.exports = ({ env })=> ({
         // CLOUDINARY_URL=cloudinary://842755312754361:BMO579r1fQKABRKoV3bHRbmd7e4@ivhfizons
-     upload:{
-         config:{
-             provider:'cloudinary',
-             providerOptions:{
-                 cloud_name:env('CLOUDINARY_NAME'),
-                 api_key: env('CLOUDINARY_KEY'),
-                 api_secret: env('CLOUDINARY_SECRET'),
-                 upload_preset: env('CLOUDINARY_UPLOAD_PRESET'),
-             },
-             actionOptions:{
-                 upload:{},
-                 uploadStream: {
-                    folder: "ENGIRICENTER",
-                 },
-                 delete:{},
-                 
-             },
-         },
-     },
+        upload: {
+          provider: 'uploadcare',
+          providerOptions: {
+            public_key: env('UPLOADCARE_PUBLIC_KEY'),
+            secret_key: env('UPLOADCARE_SECRET_KEY'),
+          },
+        },
     
     
 
